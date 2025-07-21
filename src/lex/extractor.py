38be0539cc1c3ab656a6c446e0a41c81d0981f06
samuel sipikal <samuel.sipikal@resistant.ai>
2025-07-21 10:24:31 +0200

    Add question regex

--- src/lex/extractor.py
+++ src/lex/extractor.py
@@ -6,7 +6,7 @@ from lex.lib.regex import RegexExpression
 class Extractor:
     @staticmethod
     def extract(text: str, pattern: RegexExpression) -> str:
-        match = re.search(pattern, text, re.DOTALL)
+        match = re.search(pattern, text, re.DOTALL | re.MULTILINE)
         if not match:
-            raise ValueError('Pattern not found in text.')
+            return ''
         return match.group(1).strip()
--- src/lex/lib/regex.py
+++ src/lex/lib/regex.py
@@ -4,3 +4,5 @@ RegexExpression = NewType('RegexExpression', str)
 
 
 ANAMNESIS_REGEX = r'\*\*Anamnesis:\*\*\n+(.*?)\n(?=(\*{2}|$))'
+
+QUESTION_REGEX = r'^(.*?)(?=^\s*---\s*$)'
