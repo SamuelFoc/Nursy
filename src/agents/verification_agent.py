b5cede5f71515999c9218368229881aad47f0bc7
samuel sipikal <samuel.sipikal@resistant.ai>
2025-07-21 10:24:08 +0200

    Add system prompt parameter to verification agent

--- src/agents/verification_agent.py
+++ src/agents/verification_agent.py
@@ -1,28 +1,24 @@
 import openai
 
-from communication.lib.prompts import ANSWER_VERIFICATION_PROMPT
+from communication.communication import Prompt
 
 
 class VerificationAgent:
-    def __init__(self, model='gpt-4', temperature=0, use_offline: bool = False):
+    def __init__(self, system_prompt: Prompt, model='gpt-4', temperature=0, use_offline: bool = False):
         self.model = model
         self.temperature = temperature
+        self.system_prompt = system_prompt
         self.use_offline = use_offline
 
     def verify_input(self, question: str, user_input: str) -> str:
         if self.use_offline:
             return 'True'
 
-        ANSWER_VERIFICATION_PROMPT.set_context(
-            question=question,
-            user_input=user_input,
-        )
-
-        response = openai.ChatCompletion.create(
+        response = openai.chat.completions.create(
             model=self.model,
-            messages=[{'role': 'user', 'content': ANSWER_VERIFICATION_PROMPT.render()}],
+            messages=[{'role': 'user', 'content': self.system_prompt.eval(question=question, user_input=user_input)}],
             temperature=self.temperature,
         )
 
-        str_bool = response.choices[0].message['content'].strip()
+        str_bool = response.choices[0].message.content
         return str_bool == 'True'
